b9a5387de71e35fa2856d2372b7f9d698b26b6d4
jervis535 <jervishuang123@gmail.com>
2024-04-10 20:12:59 +0700

    2

--- src/api/components/users/users-controller.js
+++ src/api/components/users/users-controller.js
@@ -50,6 +50,11 @@ async function createUser(request, response, next) {
     const name = request.body.name;
     const email = request.body.email;
     const password = request.body.password;
+    const confirmpassword=request.body.confirmpassword;
+
+    if (password!=confirmpassword) {
+      throw errorResponder(errorTypes.INVALID_PASSWORD);
+    }
 
     const success = await usersService.createUser(name, email, password);
     if (success === false) {
